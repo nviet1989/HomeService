0f805ecb8211bcfd699378855ff2a64b00fa5442
Nguyễn <nviet1989.facebook@gmail.com>
2025-09-28 22:18:01 +0700

    Fix

--- web/script.js
+++ web/script.js
@@ -11,10 +11,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const menuToggle = document.querySelector(".menu-toggle");
     const nav = document.querySelector("nav");
+    const overlay = document.createElement("div");
+    overlay.classList.add("menu-overlay");
+    document.body.appendChild(overlay);
 
     menuToggle.addEventListener("click", function () {
-        menuToggle.classList.toggle("active");
-        nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+        nav.classList.toggle("active");
+        overlay.classList.toggle("active");
+    });
+
+    overlay.addEventListener("click", function () {
+        nav.classList.remove("active");
+        overlay.classList.remove("active");
     });
 
     const tabs = document.querySelectorAll(".tab-navigation a");
